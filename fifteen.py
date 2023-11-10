9d6f6168a009578681bcd7272e51afb323a1df60
asp78 <asp78@yandex.ru>
2023-11-10 14:05:06 +0300

    2

--- fifteen.py
+++ fifteen.py
@@ -1,27 +1,60 @@
 x = 15
 while x > 0:
+    # ход игрока
+
     a = int(input('Введите цифру 1, 2 или 3: '))
     if (a == 1 or a == 2 or a == 3) and x >= a:
         x -= a
+
+    # конец игры на ходе игрока
         if x > 0:
             print(f'Осталось: {x}')
         else:
-            print('Игра окончена!')
+            print('Игра окончена! Вы проиграли')
+            break
 
-        if a == 1:
+    # ход программы
+        if x == 14:
             print('Я беру 1')
             x -= 1
-        elif a == 2:
-            print('Я беру 1')
-            x -= 1
-        elif a == 3:
+        elif x == 12:
             print('Я беру 3')
             x -= 3
+        elif x == 11:
+            print('Я беру 2')
+            x -= 2
+        elif x == 10:
+            print('Я беру 1')
+            x -= 1
+        elif x == 8:
+            print('Я беру 3')
+            x -= 3
+        elif x == 7:
+            print('Я беру 2')
+            x -= 2
+        elif x == 6:
+            print('Я беру 1')
+            x -= 1
+        elif x == 4:
+            print('Я беру 3')
+            x -= 3
+        elif x == 3:
+            print('Я беру 2')
+            x -= 2
+        elif x == 2:
+            print('Я беру 1')
+            x -= 1
+        else:
+            print('Я беру 1')
+            x -= 1
 
+
+    # конец игры на ходе программы
         if x > 0:
             print(f'Осталось: {x}')
         else:
-            print('Игра окончена!')
+            print('Игра окончена! Вы выиграли')
+
     else:
         print('Некорректный ввод')
         continue
