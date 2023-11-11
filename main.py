60e05d3df581857a3a4ca6ebf9c37e3bdba70f2a
asp78 <asp78@yandex.ru>
2023-11-12 02:44:38 +0300

    final

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ import sys
 from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog
 from random import randint
+
 var = {1: 1,
        2: 1,
        3: 2,
        4: 3,
-       5: randint(1, 3),
+    #    5: randint(1, 3),
+       5: 3,
        6: 1,
        7: 2,
        8: 3,
@@ -32,7 +34,7 @@ class App(QMainWindow):
     def sel(self):
 
         global x
-        print(f'x={x}')
+
         l = [0,
             self.c1_1.isChecked(),
             self.c1_2.isChecked(),
@@ -51,48 +53,54 @@ class App(QMainWindow):
             self.c1_15.isChecked(),]
 
 
+
     # ход игрока
         b = sum(l) # кол оставшихся галок (True) после хода игрока
         a = x - b # вычесляем кол галок взятых (False) игроком (x - было до хода игрока)
-        log.append(f'Игрок выбрал {a}\n')
-        print(f'Игрок выбрал {a}')
-        # x -= a # вычисляем кол оставшихся галок (True) после хода игрока
-        x = b # вычисляем кол оставшихся галок (True) после хода игрока
-        print(f'b={b} x={x}')
 
-        for i, j in enumerate(l, start=0):
-            if not j and i != 0:
-                exec(f'self.c1_{i}.setCheckable(False)')
+        if a == 0:
+            log.append(f'Вы ничего не взяли, выберете 1, 2 или 3 "галочки" \n')
+        elif a > 3:
+            log.append(f'Вы взяли слишком много, верните минимум {a-3} обратно \n')
+        elif b == 0:
+            log.append(f'Последнюю брать нельзя, верните обратно \n')
+
+        else:
+
+            log.append(f'Вы взяли: {a}\n')
+            # x -= a # вычисляем кол оставшихся галок (True) после хода игрока
+            x = b # вычисляем кол оставшихся галок (True) после хода игрока
+
+            for i, j in enumerate(l, start=0):
+                if not j and i != 0:
+                    exec(f'self.c1_{i}.setCheckable(False)')
 
     # ход программы
-        k = 0
-        for i, j in enumerate(l, start=0):
-            if j and i != 0 and k < var[x]:
-                k +=1
-                exec(f'self.c1_{i}.setChecked(False)')
-                exec(f'self.c1_{i}.setCheckable(False)')
-
-        log.append(f'Я беру {var[x]}\n')
-        print(f'Я беру {var[x]}') # var[x] - кол взятых галок (False) программой
-        x -= var[x] # вычисляем кол оставшихся галок (True) после хода программы
-        log.append(f'Осталось: {x}\n')
-        print(f'Осталось: {x}')
+            k = 0
+            for i, j in enumerate(l, start=0):
+                if j and i != 0 and k < var[x] and b!= 1:
+                    k += 1
+                    exec(f'self.c1_{i}.setChecked(False)')
+                    exec(f'self.c1_{i}.setCheckable(False)')
 
 
-     # конец игры
-        if x == 1:
-            print(f'Осталось: 1')
-            self.log.setText(f'Осталось {1}')
-            print('Игра окончена! Вы проиграли')
-            self.select.setText('Вы проиграли')
-            self.select.setEnabled(False)
-            self.reset.setEnabled(True)
+            log.append(f'Я беру: {var[x]}\n')
+            x -= var[x] # вычисляем кол оставшихся галок (True) после хода программы # var[x] - кол взятых галок (False) программой
 
-        elif x == 0:
-            print('Игра окончена! Вы выиграли')
-            self.select.setText('Вы выиграли')
-            self.select.setEnabled(False)
-            self.reset.setEnabled(True)
+            log.append(f'Осталось: {x}\n')
+
+
+
+    # конец игры
+            if x == 1:
+                log.append('Игра окончена! Вы проиграли')
+                self.select.setEnabled(False)
+                self.reset.setEnabled(True)
+
+            elif x == 0:
+                log.append('Игра окончена! Вы выиграли')
+                self.select.setEnabled(False)
+                self.reset.setEnabled(True)
 
     # логирование
         self.log.setText(''.join(log))
